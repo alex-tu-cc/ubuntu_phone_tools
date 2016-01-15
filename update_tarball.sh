ad90f4bffa70a7d691933bf19f0b9f8d32d5a901
Alex Tu <alex.tu@canonical.com>
2016-01-15 19:45:35 +0800

    take care Turbo, because adb reboot have some issue there.

--- update_tarball.sh
+++ update_tarball.sh
@@ -19,8 +19,11 @@ adb shell "echo update $1 >> /cache/recovery/ubuntu_command"
 adb shell "echo umount system >> /cache/recovery/ubuntu_command"
 # skip signature checking:
 adb shell mkdir -p /etc/system-image
-adb shell touch /etc/system-image/skip-gpg-verification
+# This is useless, because it will not exist in next reboot.
+# adb shell touch /etc/system-image/skip-gpg-verification
 # reboot to recovery and wait upgrader done.
+echo "now reboot to recovery..."
+echo "If your device is Turbo, please reboot to recovery manually by power+up"
 adb reboot recovery
 wait_recovery
 while [ 1 ]; do
@@ -29,5 +32,6 @@ echo wait upgrader...
 sleep 3
 done
 echo "updated $1, reboot device"
+echo "If your device is Turbo, please reboot manually"
 sleep 2
 adb reboot 
