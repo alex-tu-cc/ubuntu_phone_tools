77032ac022767a52c0fe1db168ae63f4d0f2cf0b
Alex Tu <alex.tu@canonical.com>
2016-03-10 14:51:49 +0800

    add a script to pull all logs which generated during operation

---
+++ adb_pull_logs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+if [ $1 == "--help" ]; then
+echo << EOF
+    A small script to get logcat, /var/log/*, /home/phablet/.cache/upstart/*
+    usage: $(basename $0)
+        This script assume your password on device is '1111'.
+        Then get logcat to logcat.log in background.
+        In the mean while, you can try to reproduce the issue,
+        Then press anykey to let script continue to get all logs
+EOF
+fi
+
+logs=`mktemp -d backup.XXXXX`
+pushd $logs
+# get logcat
+adb shell '/bin/echo -e "#!/bin/sh\necho 1111" >/tmp/askpass'
+adb shell "chmod +x /tmp/askpass"
+adb shell "SUDO_ASKPASS=/tmp/askpass sudo -A /system/bin/logcat -c"
+adb shell "SUDO_ASKPASS=/tmp/askpass sudo -A /system/bin/logcat" > logcat.log &
+PID=$! 
+echo "getting logcat into logcat.log ...."
+echo "press anykey to stop logcat and get logs under /var/log and ~/.cache/upstart ..... "
+read
+adb pull /var/log
+adb pull /home/phablet/.cache/upstart
+kill $PID
+echo "All logs are in $logs"
